1c51bec15cdc0d5e2d8cb8a600ba3efdcda32371
fiona <omburafiona@gmail.com>
2024-09-14 10:28:35 +0300

    pizza restaurant completed

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response,jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -23,6 +23,53 @@ api = Api(app)
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+
+class Restaurants(Resource):
+    def get(self):
+        restaurants = [restaurant.to_dict(only=('id', 'name', 'address')) for restaurant in Restaurant.query.all()]
+        return make_response(jsonify(restaurants), 200)
+class RestaurantById(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.get(id)
+        if restaurant:
+            return make_response(jsonify(restaurant.to_dict(rules=('-pizzas',))), 200)
+        else:
+            return make_response(jsonify({"error": "Restaurant not found"}), 404)
+
+    def delete(self, id):
+        restaurant = Restaurant.query.get(id)
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+            return make_response('', 204)
+        else:
+            return make_response(jsonify({"error": "Restaurant not found"}), 404)
+
+class Pizzas(Resource):
+    def get(self):
+        pizzas = [pizza.to_dict(only=('id', 'name', 'ingredients')) for pizza in Pizza.query.all()]
+        return make_response(jsonify(pizzas), 200)
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            new_restaurant_pizza = RestaurantPizza(
+                price=data['price'],
+                pizza_id=data['pizza_id'],
+                restaurant_id=data['restaurant_id']
+            )
+            db.session.add(new_restaurant_pizza)
+            db.session.commit()
+            return make_response(jsonify(new_restaurant_pizza.to_dict()), 201)
+        except ValueError as e:
+            return make_response(jsonify({"errors": ["validation errors"]}), 400)
+
+api.add_resource(Restaurants, '/restaurants')
+api.add_resource(RestaurantById, '/restaurants/<int:id>')
+api.add_resource(Pizzas, '/pizzas')
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
+
 
 
 if __name__ == "__main__":
